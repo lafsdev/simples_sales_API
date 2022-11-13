4a812df95e93a07195c86c5133e374eaf7b352a5
Leandro <lafs.dev@gmail.com>
2022-11-13 00:41:21 -0300

    Salvando e recuperando clientes

--- src/main/java/io/github/com/lafsdev/VendasApplication.java
+++ src/main/java/io/github/com/lafsdev/VendasApplication.java
@@ -1,12 +1,33 @@
 package io.github.com.lafsdev;
 
+import io.github.com.lafsdev.domain.entity.Cliente;
+import io.github.com.lafsdev.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Leandro Alves de Fontes Silva");
+            clientes.salvar(cliente);
+
+            clientes.salvar(new Cliente("Cliente 2"));
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
