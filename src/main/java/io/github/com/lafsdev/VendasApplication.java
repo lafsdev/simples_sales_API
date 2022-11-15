dc30a00eee8331edf5d587bfe85e40c0b54f179c
Leandro <lafs.dev@gmail.com>
2022-11-14 23:33:08 -0300

    Refatorado repositório de Clientes

--- src/main/java/io/github/com/lafsdev/VendasApplication.java
+++ src/main/java/io/github/com/lafsdev/VendasApplication.java
@@ -19,31 +19,31 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Leandro Alves de Fontes Silva"));
-            clientes.salvar(new Cliente("Cliente 2"));
+            clientes.save(new Cliente("Leandro Alves de Fontes Silva"));
+            clientes.save(new Cliente("Cliente 2"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
+            List<Cliente> todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Atualizando clientes.");
             todosClientes.forEach(c -> {
                 c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
+                clientes.save(c);
             });
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Buscando clientes.");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
+            clientes.findByNomeLike("Cli").forEach(System.out::println);
 
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c.getId());
+            clientes.findAll().forEach(c -> {
+                clientes.delete(c);
             });
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             if (todosClientes.isEmpty()) {
                 System.out.println("Nenhum cliente encontrado");
             } else {
--- src/main/java/io/github/com/lafsdev/domain/repository/Clientes.java
+++ src/main/java/io/github/com/lafsdev/domain/repository/Clientes.java
@@ -1,74 +1,13 @@
 package io.github.com.lafsdev.domain.repository;
 
 import io.github.com.lafsdev.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-@Repository
-public class Clientes {
-    private static String SELECT_ALL = "select * from cliente";
-    private static String DELETE = "delete from cliente  where id = ?";
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private EntityManager entityManager;
 
-    @Transactional
-    public Cliente salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Transactional
-    public Cliente atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(int id) {
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpql = "Select c from Cliente c where c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome","%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional
-    public List<Cliente> obterTodos() {
-        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
-
-    private static RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                return new Cliente(id, nome);
-            }
-        };
-    }
+    List<Cliente> findByNomeLike(String nome);
 }
