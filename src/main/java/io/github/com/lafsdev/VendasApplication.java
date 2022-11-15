76ad3a6784cac24d539009e82375a4177a0cba45
Leandro <lafs.dev@gmail.com>
2022-11-15 15:13:27 -0300

    Criado método get com parâmetros e response entity

--- src/main/java/io/github/com/lafsdev/VendasApplication.java
+++ src/main/java/io/github/com/lafsdev/VendasApplication.java
@@ -1,13 +1,26 @@
 package io.github.com.lafsdev;
 
 
+import io.github.com.lafsdev.domain.entity.Cliente;
+import io.github.com.lafsdev.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+        return args -> {
+            Cliente c = new Cliente("Leandro");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
