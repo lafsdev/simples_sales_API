d723b18cd2ca7a2db57d367d70258d62a70fb20e
Leandro <lafs.dev@gmail.com>
2022-11-15 14:41:35 -0300

    Testando Controller de clientes

--- src/main/java/io/github/com/lafsdev/VendasApplication.java
+++ src/main/java/io/github/com/lafsdev/VendasApplication.java
@@ -1,45 +1,13 @@
 package io.github.com.lafsdev;
 
-import io.github.com.lafsdev.domain.entity.Cliente;
-import io.github.com.lafsdev.domain.entity.Pedido;
-import io.github.com.lafsdev.domain.repository.Clientes;
-import io.github.com.lafsdev.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes, @Autowired Pedidos pedidos) {
-        return args -> {
-            System.out.println("Salvando clientes");
-            Cliente c = new Cliente("Leandro Alves de Fontes Silva");
-            clientes.save(c);
-
-            Pedido p = new Pedido();
-            p.setCliente(c);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedidos(c.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            pedidos.findByCliente(c).forEach(System.out::println);
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
