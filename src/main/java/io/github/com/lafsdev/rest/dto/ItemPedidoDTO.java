d5ebf609adde311aaae240159acd93c8c477cb6a
Leandro <lafs.dev@gmail.com>
2022-11-15 22:03:53 -0300

    Criados ItemPedidoDTO e PedidoDTO

---
+++ src/main/java/io/github/com/lafsdev/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,11 @@
+package io.github.com.lafsdev.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ItemPedidoDTO {
+
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+}
---
+++ src/main/java/io/github/com/lafsdev/rest/dto/PedidoDTO.java
@@ -0,0 +1,7 @@
+package io.github.com.lafsdev.rest.dto;
+
+public class PedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
