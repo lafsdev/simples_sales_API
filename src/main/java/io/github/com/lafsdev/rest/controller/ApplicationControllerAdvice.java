eea67d1cb7dc11e3f6db2c74e66a29d5b29e6e43
Leandro <lafs.dev@gmail.com>
2022-11-16 21:38:07 -0300

    Adicionado ExceptionHandlers

---
+++ src/main/java/io/github/com/lafsdev/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.github.com.lafsdev.rest.controller;
+
+import io.github.com.lafsdev.exception.RegraNegocioException;
+import io.github.com.lafsdev.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+}
