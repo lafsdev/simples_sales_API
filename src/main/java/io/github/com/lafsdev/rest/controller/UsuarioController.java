4e3aca680a83298e51fbcd07f180dc63e0b45d24
Leandro <lafs.dev@gmail.com>
2022-11-20 19:48:32 -0300

    Implementado o cadastro de usuários

---
+++ src/main/java/io/github/com/lafsdev/rest/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package io.github.com.lafsdev.rest.controller;
+
+import io.github.com.lafsdev.domain.entity.Usuario;
+import io.github.com.lafsdev.service.impl.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl  usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvar(@RequestBody @Valid Usuario usuario){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+         return usuarioService.salvar(usuario);
+    }
+}
--- src/main/java/io/github/com/lafsdev/service/impl/UsuarioServiceImpl.java
+++ src/main/java/io/github/com/lafsdev/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package io.github.com.lafsdev.service.impl;
 
+import io.github.com.lafsdev.domain.entity.Usuario;
+import io.github.com.lafsdev.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,19 +9,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
 
     @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
     private PasswordEncoder encoder;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados"));
 
-        if (!username.equals("cicrano")) {
-            throw new UsernameNotFoundException("Usuário não encontrado na base");
-        }
-        return User.builder().username("cricrano").password(encoder.encode("123")).roles("USER", "ADMIN").build();
+        String[] roles = usuario.isAdmin() ? new String[]{"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User.builder().username(usuario.getLogin()).password(usuario.getSenha()).roles(roles).build();
     }
 }
