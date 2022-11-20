b5a579a7d04718ed05c563a62d001454cc3ad1ec
Leandro <lafs.dev@gmail.com>
2022-11-20 19:52:58 -0300

    Finalizada autenticação básica

--- src/main/java/io/github/com/lafsdev/config/SecurityConfig.java
+++ src/main/java/io/github/com/lafsdev/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.com.lafsdev.config;
 import io.github.com.lafsdev.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests().antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/produtos/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/usuarios/**").permitAll()
+                .anyRequest().authenticated()
                 .and().httpBasic();
     }
 }
