100f1c578a304b0f10dc98163cb4da4d0ab5171f
Leandro <lafs.dev@gmail.com>
2022-11-20 18:38:16 -0300

    Simulando usuário do BD

--- src/main/java/io/github/com/lafsdev/config/SecurityConfig.java
+++ src/main/java/io/github/com/lafsdev/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.com.lafsdev.config;
 
+import io.github.com.lafsdev.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -18,7 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano").password(passwordEncoder().encode("123")).roles("USER");
+        // auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano").password(passwordEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -26,6 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests().antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/produtos/**").hasRole("ADMIN")
-                .and().formLogin();
+                .and().httpBasic();
     }
 }
