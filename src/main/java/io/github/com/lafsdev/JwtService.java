3f25fc3d73322d19d916b4087114207285a63f24
Leandro <lafs.dev@gmail.com>
2022-11-21 21:54:38 -0300

    Decodificando o Token

--- src/main/java/io/github/com/lafsdev/JwtService.java
+++ src/main/java/io/github/com/lafsdev/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.com.lafsdev;
 
 import io.github.com.lafsdev.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,11 +21,30 @@ public class JwtService {
     @Value("{security.jwt.chave-assinatura}")
     private String chaveAssinatura;
 
-    public String gerarToken(Usuario usuario) {
+    private String gerarToken(Usuario usuario) throws ExpiredJwtException {
         long expString = Long.valueOf(expiracao);
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
         return Jwts.builder().setSubject(usuario.getLogin()).setExpiration(data).signWith(SignatureAlgorithm.HS512, chaveAssinatura).compact();
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data = dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+
+    public Claims obterClaims(String token) {
+        return Jwts.parser().setSigningKey(chaveAssinatura).parseClaimsJwt(token).getBody();
     }
 }
