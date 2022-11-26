0cfe02e17f4af53429bf089eeed9b7f76c371b1c
Leandro <lafs.dev@gmail.com>
2022-11-25 21:33:14 -0300

    Adicionado autenticação ao swagger

--- src/main/java/io/github/com/lafsdev/config/SwaggerConfig.java
+++ src/main/java/io/github/com/lafsdev/config/SwaggerConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,7 +21,13 @@ public class SwaggerConfig {
 
     @Bean
     public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select().apis(RequestHandlerSelectors.basePackage("io.github.com.lafsdev.rest.controller")).paths(PathSelectors.any()).build().apiInfo(apiInfo());
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .select().apis(RequestHandlerSelectors.basePackage("io.github.com.lafsdev.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
@@ -26,6 +36,24 @@ public class SwaggerConfig {
 
     private Contact contact() {
         return new Contact("Leandro", "github.com/lafsdev", "lafs.dev@gmail.com");
+    }
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "acessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
     }
 
 }
